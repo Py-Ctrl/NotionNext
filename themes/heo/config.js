db963ead7105d6b630f9de69f97c68c384bccffe
Py-Ctrl <gtjbak@outlook.com>
2025-01-10 20:17:24 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,12 +4,12 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2023-7-23', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://msgzs.cloudns.org' },
+    { title: '网站会更新更多功能的！', url: 'https://msgzs.cloudns.org' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,19 +18,19 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_1: '分享技术、资源',
+  HEO_HERO_TITLE_2: '与破解',
+  HEO_HERO_TITLE_3: 'MSGZS.CLOUDNS.ORG',
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_5: '主题开放',
+  HEO_HERO_TITLE_LINK: 'https://msgzs.cloudns.org',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '技术分享', url: '/tag/技术分享' },
+  HEO_HERO_CATEGORY_2: { title: 'APK分享', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_3: { title: 'Windows软件分享', url: '/tag/Windows软件分享' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -40,12 +40,11 @@ const CONFIG = {
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
+    '🤝 分享与热心帮助',
+    '🛠️ 自研一些小程序',
     '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '🚫 就到这了吧'
+    '你干嘛哎呦！'
   ],
 
   // 个人资料底部按钮
@@ -117,10 +116,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '欢迎关注、三连投喂',
+  HEO_SOCIAL_CARD_TITLE_2: '求求了！',
+  HEO_SOCIAL_CARD_TITLE_3: '点击跳转B站个人主页',
+  HEO_SOCIAL_CARD_URL: 'https://space.bilibili.com/168058198',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
